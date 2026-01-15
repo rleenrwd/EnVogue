36743b328d854375917576331d3deff5be98efda
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-15 14:12:54 -0800

    Implented the getServices controller function.

--- server/controllers/serviceController.js
+++ server/controllers/serviceController.js
@@ -1,7 +1,26 @@
 const Service = require('../models/Service');
 
-exports.getServices = (req, res) => {
-    res.send('All Services');
+exports.getServices = async (req, res) => {
+    try {
+        const services = await Service.find({}).lean();
+
+        return res.status(200).json(
+            {
+                success: true, 
+                data: services
+            }
+        );
+    } catch (err) {
+        console.error(err.stack);
+        
+        res.status(500).json(
+            {
+                success: false,
+                message: "Internal server error.",
+            });
+        
+        
+    }
 }
 
 exports.getServiceById = (req, res) => {
