4aa76d1df26bf69f60fdcbb963305ec35a610478
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-15 18:21:25 -0800

    Added actual logic to the getServiceById controller func.

--- server/controllers/serviceController.js
+++ server/controllers/serviceController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Service = require('../models/Service');
 
 exports.getServices = async (req, res) => {
@@ -23,8 +24,38 @@ exports.getServices = async (req, res) => {
     }
 }
 
-exports.getServiceById = (req, res) => {
-    res.send('Here is the service by its ID')
+exports.getServiceById = async (req, res) => {
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid service id.'
+        });
+    }
+
+    try {
+        const service = await Service.findById(id).lean();
+
+        if (!service) {
+            return res.status(404).json({
+                success: false,
+                message: 'Service not found.'
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: service
+        });
+
+    } catch (err) {
+        console.error(err.stack);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error.'
+        });
+    }
 }
 
 exports.createService = (req, res) => {
