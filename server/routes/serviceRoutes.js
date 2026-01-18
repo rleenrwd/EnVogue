d371fd116cb2c7f57ea9b5707a383a5361d68d69
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-18 00:42:05 -0800

    Added the requireAuth and adminOnly middleware to the routes that allow writing.

--- server/routes/serviceRoutes.js
+++ server/routes/serviceRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const service = require('../controllers/serviceController');
+const requireAuth = require('../middleware/requireAuth');
+const adminOnly = require('../middleware/adminOnly');
 
 router.get('/', service.getServices);
 
 router.get('/:id', service.getServiceById);
 
-router.post('/', service.createService);
+router.post('/', [requireAuth, adminOnly], service.createService);
 
-router.put('/:id', service.updateService);
+router.put('/:id', [requireAuth, adminOnly], service.updateService);
 
-router.delete('/:id', service.deleteService);
+router.delete('/:id', [requireAuth, adminOnly], service.deleteService);
 
 module.exports = router;
