0c4384144ea3108bc8c7e4a9c381db51eeca5843
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-01-31 00:07:10 -0800

    Added controller logic to the adminCreateBooking controller.

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -223,5 +223,87 @@ exports.getBookings = async (req, res) => {
 }
 
 exports.adminCreateBooking = async (req, res) => {
-    // const {error, value} = 
+    const {error, value} = createBookingSchema.validate(req.body,{
+        abortEarly: true,
+        stripUnknown: true
+    });
+
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: error.details[0].message
+        });
+    }
+
+    try {
+        const {serviceId, date, time, customerName, phone, email, notes} = value;
+
+        const service = await Service.findById(serviceId).lean();
+
+        if (!service) {
+            return res.status(404).json({
+                success: false,
+                message: 'Service not found.'
+            });
+        }
+
+        const booking = await Booking.create({
+            serviceId,
+            date,
+            time,
+            customerName,
+            phone,
+            email,
+            notes,
+            status: 'Confirmed'
+        });
+
+        const smsConfirmation = buildBookingConfirmationSms({
+            customerName,
+            serviceName: service.name,
+            date,
+            time
+        });
+
+        
+        if (process.env.NODE_ENV !== 'production') {
+            console.log('SMS payload about to be sent:', {to: phone, body: smsConfirmation});
+        }
+
+        try {
+            await sendBookingConfirmationSms({to: phone, body: smsConfirmation});
+
+            await Booking.findByIdAndUpdate(booking._id, {
+                smsStatus: 'Sent',
+                smsError: '',
+            });
+        } catch (smsErr) {
+            console.error('SMS send failed:', smsErr);
+
+            await Booking.findByIdAndUpdate(booking._id, {
+                smsStatus: 'Failed',
+                smsError: smsErr?.message || 'SMS Failed.'
+            });
+        }
+
+        const updatedBooking = await Booking.findById(booking._id).lean();
+
+        return res.status(201).json({
+            success: true,
+            data: updatedBooking
+        });
+    } catch (err) {
+        if (err && err.code === 11000) {
+            return res.status(409).json({
+                success: false,
+                message: 'Slot already booked.'
+            });
+        }
+
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error.'
+        });
+    }
 }
