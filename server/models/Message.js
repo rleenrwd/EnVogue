49ce14fe4eaf46edbd83172c06f3945d05e476de
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-02-03 22:42:45 -0800

    Created the message model schema.

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,1 +1,14 @@
-console.log('Starting this next');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    name: {type: String, required: true, trim: true},
+    phone: {type: String, required: true, trim: true},
+    email: {type: String, required: true, trim: true},
+    message: {type: String, required: true, minlength: 10, trim: true },
+},
+{timestamps: true});
+
+
+
+module.exports = mongoose.model('Message', messageSchema);
