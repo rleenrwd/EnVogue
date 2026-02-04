478d1e65a59bedbdf64ec80da797e293f9f5da3c
rleenrwd <norwoodtheprogrammer@gmail.com>
2026-02-03 23:25:05 -0800

    Tightened phone validation.

--- server/validators/messageValidator.js
+++ server/validators/messageValidator.js
@@ -6,10 +6,21 @@ const createMessageSchema = Joi.object({
         'string.min': 'Name must be at least 2 characters.',
         'string.max': 'Name must be 80 characters or less.'
     }),
-    phone: Joi.string().min(10).max(20).trim().required().messages({
+    phone: Joi.string()
+    .trim()
+    .pattern(/^\+?[0-9()\-\s]+$/) // allows optional + at start and one of each char in the bracket only
+    .custom((value, helpers) => {
+        const digitsOnly = value.replace(/\D/g, ''); //replaces all occurences of non-digit chars
+        if (digitsOnly.length < 10 || digitsOnly.length > 15) {
+            return helpers.error('string.phoneDigits');
+        }
+        return value;
+    })
+    .required()
+    .messages({
         'any.required':'Phone number is required.',
-        'string.min': 'Phone number must be at least 10 characters.',
-        'string.max': 'Phone number must be 20 characters or less.'
+        'string.pattern.base': 'Phone number contains invalid characters.',
+        'string.phoneDigits': 'Phone number must contain between 10 and 15 digits.'
     }),
     email: Joi.string().email().trim().required().messages({
         'any.required': 'Email is required.',
